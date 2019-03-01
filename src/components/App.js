2adce253e2d24e7dcb81fa2cfcc678f2ad4d962f
Explorer <propokerstar90@gmail.com>
2019-03-01 22:06:24 +0200

    Init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>I am root component</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
