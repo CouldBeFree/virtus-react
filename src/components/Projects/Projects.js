12b8699589c9204f3ba01d8571fc52d1567ebb3a
Explorer <propokerstar90@gmail.com>
2019-03-02 13:22:51 +0200

    Created new components: Home, Projects, Statistics, Trello

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+    render () {
+        return (
+            <div>
+                <h1>I am the Projects component</h1>
+            </div>
+        )
+    }
+}
+
+export default Projects;
---
+++ src/components/Trello/Trello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Trello extends Component {
+    render () {
+        return (
+            <div>
+                <h1>I am the Trello component</h1>
+            </div>
+        )
+    }
+}
+
+export default Trello;
