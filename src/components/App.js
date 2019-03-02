23051be02e3c5fc357d8b1153ac4d93285f6bb76
Explorer <propokerstar90@gmail.com>
2019-03-02 13:57:20 +0200

    Created new component Chat

--- src/components/App.js
+++ src/components/App.js
@@ -16,21 +16,26 @@ class App extends Component {
     render() {
         return (
             <Router history={customHistory}>
-                <div className="wrapper">
-                    <Row>
-                        <Col xs="3">
-                            <Sidebar/>
-                        </Col>
-                        <Col xs="9">
-                            <Switch>
-                                <Route exact path="/" component={Home}/>
-                                <Route exact path="/auth" component={Auth}/>
-                                <Route exact path="/projects" component={Projects}/>
-                                <Route exact path="/statistics" component={Statistics}/>
-                                <Route exact path="/trello" component={Trello}/>
-                            </Switch>
-                        </Col>
-                    </Row>
+                <div>
+                    <div className="top-bar">
+
+                    </div>
+                    <div className="wrapper">
+                        <Row>
+                            <Col xs="3">
+                                <Sidebar/>
+                            </Col>
+                            <Col xs="9">
+                                <Switch>
+                                    <Route exact path="/" component={Home}/>
+                                    <Route exact path="/auth" component={Auth}/>
+                                    <Route exact path="/projects" component={Projects}/>
+                                    <Route exact path="/statistics" component={Statistics}/>
+                                    <Route exact path="/trello" component={Trello}/>
+                                </Switch>
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
             </Router>
         );
---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Chat extends Component {
+    render() {
+        return (
+            <div>
+                <h1>I am the chat component</h1>
+            </div>
+        )
+    }
+}
+
+export default Chat;
