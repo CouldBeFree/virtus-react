65d53325182ec10aaec8b465eb3a918d05b2adb1
Explorer <propokerstar90@gmail.com>
2019-03-09 17:42:28 +0200

    Created Inbox component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -42,7 +42,27 @@ class Home extends Component {
                 name: '12', uv: 3000, pv: 4300, amt: 2100,
             }
         ],
-        select: 'year'
+        select: 'year',
+        inbox: [
+            {
+                id: 1,
+                name: 'Michelle Stewart',
+                time: 'Today, 5:32 PM',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.'
+            },
+            {
+                id: 2,
+                name: 'Jolene Slater',
+                time: 'Today, 5:32 PM',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.'
+            },
+            {
+                id: 3,
+                name: 'Lyall Roach',
+                time: 'Today, 5:32 PM',
+                content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusm.'
+            }
+        ]
     };
 
     handleChange = event => {
@@ -139,10 +159,10 @@ class Home extends Component {
                 data: monthData
             })
         }
-        console.log(true)
     };
 
     render () {
+        const {inbox} = this.state;
         return (
             <div className="home-wrapper">
                 <Row>
@@ -171,6 +191,29 @@ class Home extends Component {
                                 </BarChart>
                             </ResponsiveContainer>
                         </div>
+                    </Col>
+                    <Col xs="3">
+                        <div className="inbox">
+                            <div className="inbox__top text-center">
+                                <p>Inbox</p>
+                            </div>
+                            { inbox.map((item) => {
+                                return (
+                                    <div className="box" key={item.id}>
+                                        <div className="box__top d-flex justify-content-between">
+                                            <p className="name">{item.name}</p>
+                                            <p className="time">{item.time}</p>
+                                        </div>
+                                        <div className="content">
+                                            <p>{item.content}</p>
+                                        </div>
+                                    </div>
+                                )
+                            }) }
+                        </div>
+                    </Col>
+                    <Col xs="3">
+
                     </Col>
                 </Row>
             </div>
