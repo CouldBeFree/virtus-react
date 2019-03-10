a9f303d99a7134dc17d6c5425c00cb16efdf6a01
Explorer <propokerstar90@gmail.com>
2019-03-10 19:21:54 +0200

    Styled calendar component

--- src/components/Home/Calendar.js
+++ src/components/Home/Calendar.js
@@ -2,23 +2,10 @@ import React from 'react';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
-const modifiers = {
-    birthday: new Date(2018, 9, 30)
-};
-
-const modifiersStyles = {
-    thursdays: {
-        color: '#ffc107',
-        backgroundColor: '#fffdee',
-    }
-};
-
 const Calendar = () => (
     <div>
         <DayPicker
             month={new Date()}
-            modifiersStyles={modifiersStyles}
-            modifiers={modifiers}
         />
     </div>
 );
