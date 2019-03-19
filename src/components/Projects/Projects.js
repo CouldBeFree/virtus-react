9ed212a611b97859d92baee2cb72742a63b8effe
Explorer <propokerstar90@gmail.com>
2019-03-19 21:54:34 +0200

    Created template for projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component }  from 'react';
+import Card from './Card';
+import HTML5Backend from 'react-dnd-html5-backend'
+import { DragDropContext } from 'react-dnd'
+const update = require('immutability-helper');
 
 class Projects extends Component {
-    render () {
-        return (
-            <div>
-                <h1>I am the Projects component</h1>
-            </div>
+    state = {
+        cards: [
+            {
+                id: 1,
+                text: 'Write a cool JS library',
+            },
+            {
+                id: 2,
+                text: 'Make it generic enough',
+            },
+            {
+                id: 3,
+                text: 'Write README',
+            },
+            {
+                id: 4,
+                text: 'Create some examples',
+            },
+            {
+                id: 5,
+                text: 'Spam in Twitter and IRC to promote it (note that this element is taller than the others)',
+            },
+            {
+                id: 6,
+                text: '???',
+            },
+            {
+                id: 7,
+                text: 'PROFIT',
+            },
+        ],
+    };
+
+    moveCard = (dragIndex, hoverIndex) => {
+        const { cards } = this.state;
+        const dragCard = cards[dragIndex];
+
+        this.setState(
+            update(this.state, {
+                cards: {
+                    $splice: [[dragIndex, 1], [hoverIndex, 0, dragCard]],
+                },
+            }),
         )
+    };
+
+    render() {
+        return (
+            <div className="Projects">
+                <header className="Projects-header">
+                    <h1 className="Projects-title">Welcome to React</h1>
+                </header>
+                <div className="Projects-intro">
+                    <div className="card-container">
+                        {this.state.cards.map((card, i) => (
+                            <Card
+                                key={card.id}
+                                index={i}
+                                id={card.id}
+                                text={card.text}
+                                moveCard={this.moveCard}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
-export default Projects;
+export default DragDropContext(HTML5Backend)(Projects);
